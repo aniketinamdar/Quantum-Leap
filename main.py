af5743e18970d2835df8fe8959a00b844c54fb45
ANIKET INAMDAR <aniketinamdar02@gmail.com>
2024-07-23 11:55:05 +0530

    made changes add a venv

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import time
 import threading
 import requests
+import sys
 
 curr = os.getcwd()
 
@@ -10,17 +11,31 @@ frontend_dir = os.path.join(curr, 'qlfrontend')
 backend_dir = os.path.join(curr, 'qlbackend')
 backend_script = 'main.py'
 
-# Provide the full path to the npm executable
 npm_path = 'C:\\Program Files\\nodejs\\npm.cmd'
+python_path = sys.executable  
+
+def create_virtualenv(venv_path):
+    print("Creating virtual environment...")
+    subprocess.run([python_path, '-m', 'venv', venv_path], cwd=backend_dir)
+
+def install_backend_requirements(venv_path):
+    print("Installing backend requirements...")
+    pip_executable = os.path.join(venv_path, 'Scripts', 'pip')
+    subprocess.run([pip_executable, 'install', '-r', os.path.join(backend_dir,'requirements.txt')], cwd=backend_dir)
+
+def install_frontend_dependencies():
+    print("Installing frontend dependencies...")
+    subprocess.run([npm_path, 'install'], cwd=frontend_dir)
 
 def start_frontend():
     print("Starting frontend...")
     frontend_process = subprocess.Popen([npm_path, 'run', 'dev'], cwd=frontend_dir)
     return frontend_process
 
-def start_backend():
+def start_backend(venv_path):
     print("Starting backend...")
-    backend_process = subprocess.Popen(['python', backend_script], cwd=backend_dir)
+    python_executable = os.path.join(venv_path, 'Scripts', 'python')
+    backend_process = subprocess.Popen([python_executable, backend_script], cwd=backend_dir)
     return backend_process
 
 def wait_for_process(proc, name):
@@ -39,9 +54,14 @@ def is_backend_ready(url='http://localhost:5000'):
         return False
 
 if __name__ == "__main__":
-    backend_process = start_backend()
+    venv_path = os.path.join(backend_dir, 'venv')
     
-    # Check if the backend is ready before starting the frontend
+    create_virtualenv(venv_path)
+    install_backend_requirements(venv_path)
+    install_frontend_dependencies()
+    
+    backend_process = start_backend(venv_path)
+    
     backend_ready = False
     while not backend_ready:
         print("Model is too big to load, please wait for a while...")
